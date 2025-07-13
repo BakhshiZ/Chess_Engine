e3c3572385071ba7e21b369dbc1b660d7c66a21a
BakhshiZ <bakhshizulfiqarr@gmail.com>
2025-07-13 15:01:16 +0200

    Added knight moves in knight.py

--- pieces/knight.py
+++ pieces/knight.py
@@ -3,16 +3,17 @@ from src.constants import MIN_INDEX, MAX_INDEX
 from typing import TYPE_CHECKING, Tuple
 if TYPE_CHECKING:
     from src.board import Board
-"""
-DDL, DDR, LLU, LLD, RRU, RRD, UUL, UUR
-"""
 
 def get_knight_moves(board: 'Board', piece_coordinate: Coordinate) -> Tuple[MoveCoordinate, ...]:
+    legal_moves = []
     directions = [
-        (-2, -1), (-2, 1), (1, -2), (-1, -2),
-        (2, 1), (2, -1), (-1, -2), (-1, 2)
+        (1, -2), (-1, -2), # LLU, LLD
+        (-2, -1), (-2, 1), # DDL, DDR
+        (2, -1), (2, 1), # UUL, UUR
+        (1, 2), (-1, 2) # RRU, RRD
     ]
     square = board.get_piece_info(piece_coordinate)
+    old_coord = piece_coordinate
 
     for d_r, d_c in directions:
         new_row = square.Row + d_r
@@ -22,4 +23,10 @@ def get_knight_moves(board: 'Board', piece_coordinate: Coordinate) -> Tuple[Move
         if not (MIN_INDEX <= new_row <= MAX_INDEX and MIN_INDEX <= new_col <= MAX_INDEX):
             continue
         
-        
+        new_coord = (new_row, new_col)
+        target_square = board.get_piece_info(new_coord)
+
+        if target_square.PieceType is None or target_square.Color != square.Color:
+            legal_moves.append((old_coord, new_coord))
+    
+    return legal_moves
